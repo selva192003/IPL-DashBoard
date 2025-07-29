a8bf6ece868a04e46b5bd0e93936555ce562d87b
selva j <dhoniselva192003@gmail.com>
2025-07-29 13:28:50 +0530

    Delete src directory

--- src/main/java/com/ipl/ipl_dashboard/repository/TeamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ipl.ipl_dashboard.repository;
-
-import com.ipl.ipl_dashboard.model.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team, String> {
-}
