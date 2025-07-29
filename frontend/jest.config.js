258c653e7b745e68eb937b5ef953350e754e54fe
selva j <dhoniselva192003@gmail.com>
2025-07-29 13:28:37 +0530

    Delete frontend directory

--- frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.(js|jsx)$": "babel-jest",
-  },
-  transformIgnorePatterns: [
-    "/node_modules/(?!axios)/", // ✅ this line makes Axios transformable
-  ],
-};
--- frontend/src/components/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Loader = () => (
-  <div style={{ textAlign: "center", padding: "20px" }}>
-    <h2>Loading...</h2>
-  </div>
-);
-
-export default Loader;
